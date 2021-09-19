61409c74b9e41a37b848eae038ad493249a2e259
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-19 15:58:32 +1000

    center all components when scrolling - gives nice effect when scrolling multiple different car items (stop from not showing the whole item)

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, Dimensions} from 'react-native';
 import CarItem from '../CarItem';
 
 import styles from './styles'
@@ -13,6 +13,18 @@ const CarsList = (props) => {
 
         // function will be called on each car in cars array
         renderItem = {({item}) => <CarItem car={item} />}
+
+        // snap to a start of a component
+        snapToAlignment = {'start'}
+
+        // to hide scrolling indicator
+        showsVerticalScrollIndicator = {false}
+
+        // speed of animation when flat list move up or down
+        decelerationRate = {'fast'}
+
+        // send values on how big item is in flatlist - which will be the height of the item which is also the height of the screen
+        snapToInterval = {Dimensions.get('window').height}
       />
     </View>
   );
