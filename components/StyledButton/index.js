7c62d487d0c590a553e00949c1aa7b0c55e3679b
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-19 14:49:20 +1000

    added button component and if button pressed, warning outputted to user

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {View, Text} from 'react-native'
+import {View, Text, Pressable} from 'react-native'
+import styles from './styles'
 
 const StyledButton = (props) => {
   return ( 
     <View>
-      <Text>
-        Just a component
-      </Text>
+      <Pressable
+        style = {styles.button}
+        onPress = {() => {
+          console.warn('button clicked')
+        }}
+      >
+
+        <Text>Custom Order</Text>
+      </Pressable>
     </View>
   );
 }
