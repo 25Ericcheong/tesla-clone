f70fa9a53fe72f1a49f1d8062b348050b11f1aa4
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-19 14:23:02 +1000

    split styles and rendering logic and use component to ensure no code duplication and use same code to display idfferent data values - import from Reach to render jsx

---
+++ components/CarItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// component called CarItem - will be reused
+const CarItem = (props) => {
+  return (
+    <View>
+      <Text>Not just a component</Text>
+    </View>
+  );
+};
+
+export default CarItem;
