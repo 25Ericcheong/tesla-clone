e8b9366d8680a9c6a236b0b05181c5ae6adbf686
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-19 14:34:30 +1000

    index.js to render everything alongn wiht style.sj for stylin

---
+++ components/StyledButton/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+const StyledButton = (props) => {
+  return ( 
+    <View>
+      <Text>
+        Just a component
+      </Text>
+    </View>
+  );
+}
+
+export default StyledButton;
